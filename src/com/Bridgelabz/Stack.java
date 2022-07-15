b9a56f7e65eb2bcda2cd2664f2fb7c64a8000b15
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-07-15 19:10:02 +0530

    Created stack using linkedlist

---
+++ src/com/Bridgelabz/Stack.java
@@ -0,0 +1,39 @@
+package com.Bridgelabz;
+import java.util.Scanner;
+public class Stack {
+    LinkedList linkedList = new LinkedList();
+    static Stack stack = new Stack();
+    static Scanner scanner = new Scanner(System.in);
+    public void push(){
+        System.out.print("Enter an item in stack : ");
+        int num = scanner.nextInt();
+        linkedList.addNode(num);
+        linkedList.print();
+    }
+    public void display() {
+        System.out.print("\nDo you want to create a stack (1/0) : ");
+        int yn = scanner.nextInt();
+        if (yn == 1) {
+            System.out.print("How many items you want to add or delete: ");
+            int no = scanner.nextInt();
+            for (int i = 1; i <= no; i++) {
+                System.out.println("\n1-> Add an item  \n0-> Stop : ");
+                int add = scanner.nextInt();
+                switch (add) {
+                    case 1 -> push();
+
+                    case 0 -> {
+                        linkedList.print();
+                        System.out.println("Thank you!");
+                    }
+                }
+            }
+        } else {
+            linkedList.print();
+            System.out.println("\nThank you!");
+        }
+    }
+    public static void main(String[] args){
+        stack.display();
+    }
+}
